932740c1b316ad21f848c8e90ad0b11130fe8b7b
Jose Dorrío <j.dorrio.rodriguez@accenture.com>
2016-11-04 15:27:04 +0100

    dale

--- src/main/java/com/jd/openshift/MicroServiceController.java
+++ src/main/java/com/jd/openshift/MicroServiceController.java
@@ -1,27 +1,22 @@
 package com.jd.openshift;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 public class MicroServiceController {
 
+	private final AddressService service;
 
-    private final AddressService service;
+	public MicroServiceController(AddressService service) {
+		this.service = service;
+	}
 
-    public MicroServiceController(AddressService service) {
-        this.service = service;
-    }
+	@RequestMapping(value = "/micro-service")
+	public String hello() throws Exception {
 
-    @RequestMapping(value = "/micro-service")
-    public String hello() throws Exception {
-
-        String serverAddress = service.getServerAddress();
-        return new StringBuilder().append("Hello from IP address: ").append(serverAddress).append("\n").toString();
-    }
-
+		String serverAddress = service.getServerAddress();
+		return new StringBuilder().append("Hello from IP address: ").append(serverAddress).append("\n").toString();
+	}
 
 }
