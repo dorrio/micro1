7af723e87491e4687c8c316d8c90be76664fdf84
Jose Dorrío <j.dorrio.rodriguez@accenture.com>
2016-11-04 14:31:54 +0100

    funcionando

--- src/main/java/com/jd/openshift/Micro1Application.java
+++ src/main/java/com/jd/openshift/Micro1Application.java
@@ -1,11 +1,16 @@
 package com.jd.openshift;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.jd.openshift")
+@Configuration
+@EnableAutoConfiguration
+@RestController
+//@ComponentScan(basePackages="com.jd.openshift")
 public class Micro1Application {
 
 	public static void main(String[] args) {
